8a5027c77b3dd60678124485656bd6efe3e92e99
Billy Baney <billybaneydev@gmail.com>
2021-10-27 17:07:36 -0500

    adding /me route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   post '/login', to: 'session#create'
   post '/signup', to: 'users#create'
   delete '/logout', to: 'session#destroy'
+  get '/me', to: 'users#show'
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
